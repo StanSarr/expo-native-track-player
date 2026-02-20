b412234e0d59f52dfe06fefa35442e4f7e7a9bb1
Stan SARR <stan.sarr@kooblit.com>
2026-02-20 07:48:43 +0100

    feat: add TrackMetadataBase interface for defining essential track metadata properties

---
+++ src/types/TrackMetadataBase.ts
@@ -0,0 +1,17 @@
+export interface TrackMetadataBase {
+  id: string;
+  title?: string;
+  artist?: string;
+  albumName?: string;
+  artworkUri?: string;
+  trackNumber?: number;
+  duration?: number;
+  composer?: string;
+  conductor?: string;
+  genre?: string;
+  compilation?: string;
+  subtitle?: string;
+  description?: string;
+  station?: string;
+  mediaType?: number;
+}
